37d2ec3f6a91ac6780f782373109cebd40f4b43d
arcadierfrontend <71870637+arcadierfrontend@users.noreply.github.com>
2021-05-06 15:02:48 +0800

    Updated url to staging
    Updated url to staging

--- Scripts/Custom Search Page/UTILITIES/OPEN BROWSER/Script1600929822962.groovy
+++ Scripts/Custom Search Page/UTILITIES/OPEN BROWSER/Script1600929822962.groovy
@@ -15,7 +15,7 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 
-WebUI.openBrowser('https://gdasandbox.test-arcadier.com')
+WebUI.openBrowser('https://diagnostics.staging.arcadier.io/')
 
 WebUI.maximizeWindow()
 
